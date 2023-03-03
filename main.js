9737ec89eb71ff193b2f1bf8c32621334f1be6c5
therandomspoon <107148755+therandomspoon@users.noreply.github.com>
2023-03-03 08:17:25 +0000

    Update main.js

--- main.js
+++ main.js
@@ -337,7 +337,7 @@ window.onload = function () {
 	}
 
 	// default goal time
-	Papiezowa.goal.setHours(8, 15, 0)
+	Papiezowa.goal.setHours(8, 20, 0)
 
 	// default events
 	Papiezowa.addOn(function () {
